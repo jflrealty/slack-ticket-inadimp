e176426d832f853fd6e1bd96521650f84da715be
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-09-19 17:57:01 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+# Pega a URL do banco do Railway
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Conexão
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base para os models
 Base = declarative_base()
+
+# Sessão
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
